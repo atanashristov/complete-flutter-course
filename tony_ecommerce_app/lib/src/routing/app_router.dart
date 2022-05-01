57516c3f1ff227376e4586145b2caf286b1cabcb
Atanas Hristov <AtanasHristov@hotmail.com>
2022-05-01 16:43:34 -0500

    Section 2: GoRouter: Extension methods and pageBuilder

--- tony_ecommerce_app/lib/src/routing/app_router.dart
+++ tony_ecommerce_app/lib/src/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tony_ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:tony_ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
@@ -12,7 +13,11 @@ final goRouter = GoRouter(
       routes: [
         GoRoute(
           path: 'cart',
-          builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true, // Controls the animation from the bottom instead from the right side.
+            child: const ShoppingCartScreen(),
+          ),
         ),
       ],
     ),
